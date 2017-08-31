e95af298d65f702b08ca0a2a36e7c80a55162a53
Roliga <roliga.here@gmail.com>
2017-08-31 13:09:01 +0200

    Upgrade to php 7

--- run-tt-rss.sh
+++ run-tt-rss.sh
@@ -10,7 +10,7 @@ setupOutput='/config.php'
 updater="$ttrssPath/update_daemon2.php"
 
 # Start the web server
-service php5-fpm start
+service php7.0-fpm start
 service nginx start
 
 if [ "$SETUP" = 'true' ]; then
@@ -40,7 +40,7 @@ else
 	updaterPID=$!
 
 	# Check that all services are running every 10 seconds, and if not kill the whole container.
-	while service php5-fpm status >/dev/null 2>&1 && \
+	while service php7.0-fpm status >/dev/null 2>&1 && \
 		service nginx status >/dev/null 2>&1 && \
 		kill -0 "$updaterPID" >/dev/null 2>&1; do
 
@@ -54,4 +54,4 @@ fi
 
 # Stop webserver
 service nginx stop
-service php5-fpm stop
+service php7.0-fpm stop
